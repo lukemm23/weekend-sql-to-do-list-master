58cca9662868cc901aa3979dbdb60481c6ea8938
lukemm23 <lukemm23@gmail.com>
2019-11-27 19:54:53 -0600

    file setup, database creation, server side set up, html/style set up

---
+++ database.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "todos" (
+    "id" SERIAL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "date" DATE
+);
+
+INSERT INTO "todos" (description, status, date)
+VALUES ('walk the dog', 'not done', '11/25/2019');
+INSERT INTO "todos" (description, status, date)
+VALUES ('buy groceries', 'not done', '11/25/2019');
+INSERT INTO "todos" (description, status, date)
+VALUES ('get gas', 'not done', '11/26/2019');
